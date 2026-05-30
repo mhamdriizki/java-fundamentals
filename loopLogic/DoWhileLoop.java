cbd7adda3d4dda6aec606c67f3372b00d5cc4499
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2026-05-30 12:02:49 +0700

    feat: While and Do While logic and see the differences

---
+++ loopLogic/DoWhileLoop.java
@@ -0,0 +1,11 @@
+package loopLogic;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 6;
+        do {
+            System.out.println("The value of i is: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
---
+++ loopLogic/WhileLoop.java
@@ -0,0 +1,11 @@
+package loopLogic;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 6;
+        while (i <= 5) {
+            System.out.println("The value of i is: " + i);
+            i++;
+        }
+    }
+}
