755a46d7ae35a6804fb2a733cb4a773aa0ff565e
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2026-05-30 12:49:37 +0700

    feat: Implied encapsulation concept

--- MainApp.java
+++ MainApp.java
@@ -1,9 +1,16 @@
-// import conditionalLogic.IfElse;
+import oop.Encapsulation;
 
 public class MainApp {
-    // public static void main(String[] args) {
-    //     System.out.println("Hello, World!");
-    //     IfElse ifElse = new IfElse();
-    //     ifElse.cekNilai();
-    // }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Constructor default value
+        Encapsulation encapsulation = new Encapsulation("John Doe", 30);
+
+        // Update data
+        encapsulation.setName("John Does");
+        encapsulation.setAge(31);
+
+        System.out.println("Name: " + encapsulation.getName());
+        System.out.println("Age: " + encapsulation.getAge());
+    }
 }
